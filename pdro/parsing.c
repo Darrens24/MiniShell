b957c591e0736f70ebc67d6eb8ad532596a2cba1
mpXq <120124005+mpXq@users.noreply.github.com>
2023-01-29 15:44:32 +0100

    norms

--- pdro/parsing.c
+++ pdro/parsing.c
@@ -6,89 +6,85 @@
 /*   By: pfaria-d <pfaria-d@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 13:04:14 by pfaria-d          #+#    #+#             */
-/*   Updated: 2022/12/22 12:58:15 by pfaria-d         ###   ########.fr       */
+/*   Updated: 2023/01/29 15:42:29 by pfaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	is_wspace(char c)
+static int	jump_text(char *line, int i)
 {
-	if (c == 32 || (c >= 9 && c <= 13))
-		return (1);
-	return (0);
-}
+	char	quote;
 
-static int	jump_wspace(char *line, int i)
-{
-	while (line[i] && is_wspace(line[i]))
+	quote = line[i++];
+	while (line[i] && line[i] != quote)
 		i++;
 	return (i);
 }
 
-static int	checknextquote(char *line, char quote, int start)
+int	firstparsing(t_toklst *tokenlst, char *line, int start, int i)
 {
-	int	i;
-
-	i = start + 1;
-	while (line[i])
-	{
-		if (line[i] == quote)
-			return (1);
+	i = jump_wspace(line, i);
+	start = i;
+	while (line[i] == '|')
 		i++;
+	if (start != i)
+		new_back_tok(tokenlst, line, start, i);
+	i = jump_wspace(line, i);
+	start = i;
+	while (line[i] == '<')
+		i++;
+	if (start != i)
+		new_back_tok(tokenlst, line, start, i);
+	i = jump_wspace(line, i);
+	start = i;
+	while (line[i] == '>')
+		i++;
+	if (start != i)
+		new_back_tok(tokenlst, line, start, i);
+	i = jump_wspace(line, i);
+	return (i);
+}
+
+int	secondparsing(t_toklst *tokenlst, char *line, int start, int i)
+{
+	int		tmp;
+
+	while (line[i] && !is_wspace(line[i]) && !is_sep(line[i]))
+	{
+		tmp = i;
+		if (line[i] == '\'' || line[i] == '\"')
+		{
+			if (checknextquote(line, line[i], tmp))
+				i = jump_text(line, i);
+			else
+				return (errorintoken(tokenlst));
+			i++;
+		}
+		else
+			while (line[i] && !is_wspace(line[i]) && !is_sep(line[i])
+				&& line[i] != '\'' && line[i] != '\"')
+				i++;
 	}
-	return (0);
+	if (start != i)
+		new_back_tok(tokenlst, line, start, i);
+	return (i);
 }
 
 void	parsing(t_toklst *tokenlst, char *line)
 {
 	int	start;
 	int	i;
-	char	quote;
-	int	tmp;
 
 	i = 0;
+	start = i;
 	while (line[i])
 	{
-		i = jump_wspace(line, i);
+		i = firstparsing(tokenlst, line, start, i);
 		start = i;
-		while (line[i] == '|')
-			i++;
-		if (start != i)
-			new_back_tok(tokenlst, line, start, i);
-		i = jump_wspace(line, i);
+		i = secondparsing(tokenlst, line, start, i);
+		if (i == -1)
+			return ;
 		start = i;
-		while (line[i] == '<')
-			i++;
-		if (start != i)
-			new_back_tok(tokenlst, line, start, i);
-		i = jump_wspace(line, i);
-		start = i;
-		while (line[i] && !is_wspace(line[i]) && line[i] != '|' && line[i] != '<')
-		{
-			tmp = i;
-		if (line[i] == '\'' || line[i] == '\"')
-		{
-			if (checknextquote(line, line[i], tmp))
-			{
-				quote = line[i++];
-				while (line[i] && line[i] != quote)
-					i++;
-			}
-			else
-			{
-				printf("no ending quote\n");
-				clear_toklst(tokenlst);
-				return ;
-			}
-			i++;
-		}
-		else
-			while (line[i] && !is_wspace(line[i])
-				&& line[i] != '|' && line[i] != '<' && line[i] != '\'' && line[i] != '\"')
-				i++;
-		}
-		if (start != i)
-			new_back_tok(tokenlst, line, start, i);
 	}
 }
--- pdro/utils.c
+++ pdro/utils.c
@@ -6,7 +6,7 @@
 /*   By: pfaria-d <pfaria-d@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 13:42:26 by pfaria-d          #+#    #+#             */
-/*   Updated: 2023/01/28 14:43:07 by pfaria-d         ###   ########.fr       */
+/*   Updated: 2023/01/29 15:05:22 by pfaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strndup(char *line, int start, int end)
 {
-	int	i;
+	int		i;
 	char	*str;
 
 	i = 0;
