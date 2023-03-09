4b360150041add37d5d00e733338e74116396cad
Edouard Leleux <eleleux@c2r6p9.42nice.fr>
2023-03-09 17:36:25 +0100

    signal

--- MinishellCommit/signals.c
+++ MinishellCommit/signals.c
@@ -6,11 +6,43 @@
 /*   By: pfaria-d <pfaria-d@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 12:13:53 by pfaria-d          #+#    #+#             */
-/*   Updated: 2023/03/08 14:33:20 by eleleux          ###   ########.fr       */
+/*   Updated: 2023/03/08 20:58:45 by eleleux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	remove_ctrlc(int mode)
+{
+	struct termios	termi;
+
+	tcgetattr(0, &termi);
+	if (mode == 1)
+		termi.c_lflag &= ~ECHOCTL;
+	else
+		termi.c_lflag |= ECHOCTL;
+	tcsetattr(0, TCSANOW, &termi);
+}
+
+void	do_nothing(int num)
+{
+	if (num == SIGINT)
+		return ;
+	if (num == SIGQUIT)
+		return ;
+}
+
+int	reset_echo(void)
+{
+	struct termios	old;
+	struct termios	new;
+
+	tcgetattr(STDIN_FILENO, &old);
+	new = old;
+	new.c_lflag |= ~(ECHOCTL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &new);
+	return (EXIT_SUCCESS);
+}
 
 void	handler(int num)
 {
@@ -23,12 +55,12 @@ void	handler(int num)
 	term.c_cc[VMIN] = 1;
 	term.c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, &term);*/
-	struct sigaction sa;
+	/*struct sigaction sa;
     sa.sa_handler = handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);*/
 
 	struct termios	saved;
 	struct termios	new;
@@ -38,10 +70,13 @@ void	handler(int num)
 	tcsetattr(STDIN_FILENO, TCSANOW, &new);
 	if (num == SIGINT)
 	{
-		rl_replace_line("", 0);
-		ft_putchar_fd('\n', 1);
-		rl_on_new_line();
-		rl_redisplay();
+		if (g_err != 130)
+		{
+			rl_replace_line("", 0);
+			ft_putchar_fd('\n', 1);
+			rl_on_new_line();
+			rl_redisplay();
+		}
 		g_err = 1;
 	}
 	if (num == SIGQUIT)
@@ -59,22 +94,25 @@ void	heredoc_handler(int num)
 	term.c_cc[VMIN] = 1;
 	term.c_cc[VTIME] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, &term);*/
-	struct sigaction sa;
+	/*struct sigaction sa;
     sa.sa_handler = heredoc_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGQUIT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);*/
 
-	struct termios	saved;
+/*	struct termios	saved;
 	struct termios	new;
 	tcgetattr(STDIN_FILENO, &saved);
 	new = saved;
 	new.c_lflag &= ~(ECHOCTL);
-	tcsetattr(STDIN_FILENO, TCSANOW, &new);
+	tcsetattr(STDIN_FILENO, TCSANOW, &new);*/
 	if (num == SIGINT)
 	{
-		g_err = 130;
+		rl_replace_line("", 0);
+		ft_putchar_fd('\n', 1);
+		rl_on_new_line();
+		g_err = 1;
 		exit(1);
 	}
 	if (num == SIGQUIT)
