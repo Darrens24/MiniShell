b5fab6ed1506ba315b123af06cf611c684ecbd3e
Pedro Faria de sousa <pfaria-d@c1r8p3.42nice.fr>
2023-04-25 18:20:15 +0200

    abandon execution

--- Minishell_Bonus/execution/exec_bonus/execution_bonus.c
+++ Minishell_Bonus/execution/exec_bonus/execution_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    execution_bonus.c                                 :+:      :+:    :+:   */
+/*   execution_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pfaria-d <pfaria-d@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 10:55:45 by pfaria-d          #+#    #+#             */
-/*   Updated: 2023/04/25 17:11:02 by pfaria-d         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:19:49 by pfaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,6 @@ void	changefds(t_shell *shell, int i)
 
 	index = -1;
 	fd = malloc(sizeof(int *) * (i - 1));
-	if (fd)
-		return (EXIT_FAILURE);
 	while (++index < i - 1)
 		fd[index] = malloc(sizeof(int) * 2);
 	index = -1;
@@ -51,11 +49,7 @@ void	changefds(t_shell *shell, int i)
 	}
 	index = -1;
 	while (++index < shell->nb_of_pipes - 1)
-	{
-		free(shell->fd[index][1]);
-		free(shell->fd[index][0]);
 		free(shell->fd[index]);
-	}
 	free(shell->fd); // peut etre a supp
 	shell->fd = fd;
 }
@@ -88,18 +82,65 @@ int	redirection_bonus(t_shell *shell)
 	return (EXIT_SUCCESS);
 }
 
+char	*get_correct_path_bonus(t_shell *shell, char *command)
+{
+	int			i;
+	struct stat	buff;
+
+	stat(command, &buff);
+	shell->correct_path = NULL;
+	i = -1;
+	while (shell->all_path[++i] && (ft_strlen(command) > 0
+			&& !S_ISDIR(buff.st_mode)))
+	{
+		shell->correct_path = ft_strjoin(shell->all_path[i],
+				command);
+		if (access(shell->correct_path, F_OK) == 0)
+			return (shell->correct_path);
+		free(shell->correct_path);
+	}
+	g_err = 127;
+	if (ft_strncmp(".", command, 1) == 0)
+		printf("%s: No such file or directory\n", command);
+	else
+		printf("%s: Command not found\n", command);
+	return (NULL);
+}
+
+char	*find_path_bonus(char *command, t_shell *shell)
+{
+	char	*temp;
+	char	*all_path;
+
+	all_path = get_path(shell->array_env);
+	if (!all_path)
+	{
+		return (printf("%s : Command not found\n",
+				command), NULL);
+	}
+	shell->all_path = ft_split_slash(all_path, ':');
+	if (!shell->all_path[0])
+		return (NULL);
+	temp = get_correct_path_bonus(shell, command);
+	free_array(shell->all_path);
+	if (!temp)
+		return (NULL);
+	return (temp);
+}
+
 int	execute_command_clean_leaf(t_shell *shell, char **command)
 {
 	char		*tmp;
 	struct stat	buff;
 
-	stat(command, &buff);
+	stat(command[0], &buff);
+	tmp = NULL;
 	if (!is_builtin_command_bonus(command))
 	{
-		if (access(command, F_OK) == 0 && S_ISREG(buff.st_mode))
-			tmp = ft_strdup(command);
-	//	else
-	//		tmp = find_path_bonus();
+		if (access(command[0], F_OK) == 0 && S_ISREG(buff.st_mode))
+			tmp = ft_strdup(command[0]);
+		else
+			tmp = find_path_bonus(command[0], shell);
 		if (!tmp)
 		{
 			if (index > 0)
@@ -110,20 +151,53 @@ int	execute_command_clean_leaf(t_shell *shell, char **command)
 		signal(SIGINT, &do_nothing);
 		signal(SIGQUIT, &do_nothing);
 		if (shell->pid[shell->index_of_pipes])
-		{
-			redirection_bonus(shell);
-			execve(tmp, command, shell->array_env);
-		}
+	//	{
+	//		redirection_bonus(shell);
+	//		execve(tmp, command, shell->array_env);
+	//	}
 	}
-	//else
-	//{
-	//	
-//	}
+	else
+	{
+		redirection_bonus(shell);
+		execute_builtin_bonus(command);
+	}
 	if (shell->index_of_pipes > 0)
 		close_fds(shell->fd[shell->index_of_pipes - 1]);
 	if (shell->nb_of_pipes == shell->index_of_pipes)
 		reset_pipes(shell);
 	return (EXIT_SUCCESS);
+}
+
+int	is_or(char *cmd)
+{
+	if (ft_strncmp(cmd, "||", 3) == 0)
+		return (1);
+	return (0);
+}
+
+int	is_and(char *cmd)
+{
+	if (ft_strncmp(cmd, "&&", 3) == 0)
+		return (1);
+	return (0);
+}
+
+t_branch	*clean(t_branch *temp)
+{
+	if (temp && temp->left)
+		return (clean(temp->left));
+	else if (temp && temp->right)
+		return (clean(temp->right));
+	else if (temp && temp->dad)
+	{
+		free_array(temp->cmd);
+		temp->left = NULL;
+		temp->right = NULL;
+		temp->left_command = NULL;
+		temp->right_command = NULL;
+		return (clean(temp->dad));
+	}
+	return (temp);
 }
 
 int	execution_bonus(t_shell *shell, t_branch *map)
@@ -162,7 +236,7 @@ int	execution_bonus(t_shell *shell, t_branch *map)
 		tmp = map;
 		map = map->dad;
 		execute_command_clean_leaf(shell, tmp->cmd);
-		return (execute_builtin_cmd(shell, map));
+		return (execution_bonus(shell, map));
 	}
 	if (map && map->dad)
 		return (execution_bonus(shell, map->dad));
