3ff6e0e197285a87a6ff4e45a6ec5eb9414ef058
Darrens24 <edouard.leleux@gmail.com>
2023-05-03 16:38:14 +0200

    edbranch for tests

---
+++ EdBonus/builtins/dir_management.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dir_management.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eleleux <eleleux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 14:29:55 by pfaria-d          #+#    #+#             */
+/*   Updated: 2023/05/03 15:36:02 by eleleux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	change_directory(t_shell *shell, const char *path)
+{
+	shell->previous_dir_path = getcwd(NULL, 0);
+	printf("previous is %s\n", shell->previous_dir_path);
+	if (chdir(path) < 0)
+	{
+		g_err = 1;
+		free(shell->previous_dir_path);
+		return (perror("chdir"), EXIT_FAILURE);
+	}
+	shell->current_dir_path = getcwd(NULL, 0);
+	if (shell->current_dir_path)
+	{
+		update_pwd(shell, shell->env_l);
+		update_pwd(shell, shell->sorted_env_l);
+		free(shell->current_dir_path);
+	}
+	if (shell->previous_dir_path)
+	{
+		update_oldpwd(shell, shell->env_l);
+		update_oldpwd(shell, shell->sorted_env_l);
+		free(shell->previous_dir_path);
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	go_to_home_directory(t_shell *shell)
+{
+	if (change_directory(shell, shell->home) != 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+int	go_to_previous_directory(t_shell *shell)
+{
+	if (!shell->previous_dir_path)
+		return (print_pwd_linux(shell));
+	if (change_directory(shell, shell->previous_dir_path) != 0)
+		return (EXIT_FAILURE);
+	print_pwd_linux(shell);
+	return (EXIT_SUCCESS);
+}
+
+int	execute_directory_cmd(t_shell *shell, int i)
+{
+	if (ft_strncmp(shell->multi_cmd[i][0], "cd", 3) == 0)
+	{
+		if ((!shell->multi_cmd[i][1])
+			|| ft_strncmp(shell->multi_cmd[i][1], "~", 2) == 0
+			|| ft_strncmp(shell->multi_cmd[i][1], "~/", 3) == 0)
+			return (go_to_home_directory(shell));
+		else if (ft_strncmp(shell->multi_cmd[i][1], "-", 2) == 0)
+			return (go_to_previous_directory(shell));
+		return (change_directory(shell, shell->multi_cmd[i][1]));
+	}
+	else if (ft_strncmp(shell->multi_cmd[i][0], "pwd", 4) == 0)
+		return (print_pwd_linux(shell));
+	return (EXIT_FAILURE);
+}
+
+int	print_pwd_linux(t_shell *shell)
+{
+	char	*directory;
+
+	directory = getcwd(NULL, 0);
+	if (!directory)
+	{
+		while (!directory)
+		{
+			free(directory);
+			change_directory(shell, "../");
+			directory = getcwd(NULL, 0);
+		}
+	}
+	ft_putendl_fd(directory, STDOUT_FILENO);
+	free(directory);
+	directory = NULL;
+	return (EXIT_SUCCESS);
+}
---
+++ EdBonus/tree/tree.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tree.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pfaria-d <pfaria-d@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 11:07:51 by eleleux           #+#    #+#             */
+/*   Updated: 2023/05/01 11:54:11 by eleleux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+static t_tok	*start_when_and_or(t_tok *temp)
+{
+	while (temp && !is_and_or(temp->var))
+	{
+		if (temp && ft_strncmp(temp->var, ")", 2) == 0)
+			return (and_or_reverse_jump_parentheses(temp));
+		temp = temp->prev;
+	}
+	return (temp);
+}
+
+static t_tok	*start_when_pipe(t_tok *temp)
+{
+	while (temp && !is_operator(temp->var))
+	{
+		if (temp && ft_strncmp(temp->var, ")", 2) == 0)
+			return (pipe_reverse_jump_parentheses(temp));
+		temp = temp->prev;
+	}
+	return (temp);
+}
+
+t_tok	*go_to_branch_start(t_toklst *user_command)
+{
+	t_tok	    *temp;
+    static int  prio = 0;
+
+	temp = user_command->end;
+	if (temp && and_or_in_cmd_outside_parentheses(user_command))
+    {
+		temp = start_when_and_or(temp);
+        if (prio)
+            temp->prio = prio;
+    }
+	else if (temp && pipe_in_cmd_outside_parentheses(user_command))
+    {
+		temp = start_when_pipe(temp);
+        if (prio)
+            temp->prio = prio;
+    }
+	else if (temp && ft_strncmp(temp->var, ")", 2) == 0)
+	{
+		remove_front_tok(user_command);
+		remove_back_tok(user_command);
+        prio++;
+		temp = go_to_branch_start(user_command);
+	}
+	return (temp);
+}
+
+//ls[0] &&[0] ((cat lol[2] ||[2] whoami[2]) |[1] grep a[1]) 
+
+t_tree	*get_new_start_split_command(t_shell *shell, t_branch *map)
+{
+	if (map && map->left_command)
+	{
+		create_left_leaf(map);
+		clear_toklst(map->left_command);
+		free(map->left_command);
+		map->left_command = NULL;
+		get_new_start_split_command(shell, map->left);
+	}
+	else if (map && map->right_command)
+	{
+		create_right_leaf(map);
+		clear_toklst(map->right_command);
+		free(map->right_command);
+		map->right_command = NULL;
+		get_new_start_split_command(shell, map->right);
+	}
+	else if (map && map->dad)
+		get_new_start_split_command(shell, map->dad);
+	return (shell->tree);
+}
+
+void	fill_trinary_tree(t_toklst *user_command, t_shell *shell)
+{
+	t_tok		*temp;
+
+	shell->tree = malloc(sizeof(t_tree));
+	if (!shell->tree)
+		return ;
+	temp = user_command->start;
+	temp = go_to_branch_start(user_command);
+	shell->tree = create_start_branch(shell->tree, temp, user_command);
+	shell->array_env = get_array_env(shell);
+	shell->home = ft_strdup(get_home(shell->array_env));
+	shell->tree = get_new_start_split_command(shell, shell->tree->map);
+}
